67559b69378d7c4a9043a241a1b698d6e71a5cea
JuliusMischok <julius.mischok@gmail.com>
2017-01-14 23:36:54 +0100

    Started implementing registration

--- registration/src/main/angularjs/app/scripts/registration.app.js
+++ registration/src/main/angularjs/app/scripts/registration.app.js
@@ -1,4 +1,6 @@
-var app = angular.module('registration', ['ngResource', 'ui.bootstrap', 'ngMessages', 'pascalprecht.translate', 'ui.router'])
-				.value('backendServiceUrl', undefined)	// Parameter 'studyflix.webgui.backendServiceUrl' from application.properties or command line. Always ends with "/"
+'use strict';
+
+var app = angular.module('registration', ['ngResource', 'ui.bootstrap', 'ngMessages', 'pascalprecht.translate'])
+				.value('backendServiceUrl', undefined)	// Parameter 'konkretlive.registration.backendServiceUrl' from application.properties or command line. Always ends with "/"
 				;
 				
--- registration/src/main/angularjs/app/scripts/registration.translations.js
+++ registration/src/main/angularjs/app/scripts/registration.translations.js
@@ -1,2 +1,27 @@
-var app = angular.module('registration');
-				
+'use strict';
+
+var app = angular.module('registration')
+	.config(['$translateProvider', function($translateProvider) {
+		$translateProvider.translations('de', {
+			HEADLINE_HINTS_START:					'Start',
+			HEADLINE_GROUP_INPUT:					'Gruppe',
+			HEADLINE_LEADER_INPUT:					'Leiter',
+			HEADLINE_PARTICIPANTS_INPUT:			'Teilnehmer',
+			HEADLINE_CONFIRM:						'Abschluss',
+			
+			FORM_LABEL_CHURCH:						'Gemeinde',
+			FORM_LABEL_GROUPTYPE:					'Gruppenart',
+			FORM_LABEL_DISTRICT:					'FeG Bundeskreis',
+			
+			FORM_PLACEHOLDER_CHURCH:				'Eure Gemeinde...',
+			FORM_PLACEHOLDER_GROUPTYPE:				'Eure Gruppenart...',
+			FORM_PLACEHOLDER_DISTRICT:				'Euer Bundeskreis...',
+			
+			BUTTON_PROCEED:							'Weiter',
+			
+			MESSAGE_VALIDATION_REQUIRED:			'Eingabe von "{{field}}" ist verpflichtend',
+			MESSAGE_VALIDATION_TOO_LONG:			'Eingegebener Wert für "{{field}}" ist zu lang',
+		});
+		
+		$translateProvider.preferredLanguage('de');
+	}]);
