6214ba8ae67bcd8963131058a1935bd6fdf28273
JuliusMischok <julius.mischok@gmail.com>
2017-01-31 23:05:24 +0100

    Started implementing participant input

--- registration/src/main/angularjs/app/scripts/registration.controllers.js
+++ registration/src/main/angularjs/app/scripts/registration.controllers.js
@@ -4,16 +4,26 @@ var app = angular.module('registration')
 	.controller('RegistrationController', ['$scope', function($scope) {
 		$scope.group = {};
 		$scope.leader = {};
+		$scope.participants = [];
+		
+		$scope.initPopovers = function() {
+			angular.element('[data-toggle="popover"]').popover();
+		};
 	}])
 
 	.controller('GroupInputController', ['$scope', 'grouptypeService', 'districtService', function($scope, grouptypeService, districtService) {
 		$scope.grouptypes = grouptypeService.grouptypes();
-		$scope.districts = districtService.districts();		
+		$scope.districts = districtService.districts();
+		
+		$scope.initPopovers();
 	}])
 	
 	.controller('LeaderInputController', ['$scope', 'priceService', function($scope, priceService) {
 		$scope.prices = priceService.prices();
 		$scope.birthdaypopup = {opened: false};
+
+		$scope.initPopovers();
+		
 		$scope.dateOptions = {
 			formatYear : 'yyyy',
 			startingDay : 1
@@ -24,6 +34,40 @@ var app = angular.module('registration')
 		};
 	}])
 	
-	
+	.controller('ParticipantInputController', ['$scope', 'priceService', function($scope, priceService) {
+		$scope.prices = priceService.prices();
+		$scope.birthdaypopup = {opened: false};
+		$scope.showList = true;
+		$scope.participant = {};
+		
+		$scope.initPopovers();
+		
+		$scope.dateOptions = {
+			formatYear : 'yyyy',
+			startingDay : 1
+		};
+		
+		$scope.openBirthdayPopup = function() {
+			$scope.birthdaypopup.opened = true;
+		};
+		
+		$scope.showAddParticipantForm = function() {
+			$scope.participant = {};
+			$scope.showList = false;
+			$scope.participantform.$setPristine();
+			$scope.participantform.$setUntouched();
+		};
+		
+		$scope.backToList = function() {
+			$scope.participant = {};
+			$scope.showList = true;
+		};
+		
+		$scope.saveParticipant = function() {
+			$scope.participants.push(angular.copy($scope.participant));
+			$scope.participant = {};
+			$scope.showList = true;
+		};
+	}])
 	;
 				
